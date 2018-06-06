47c64ebd28949101464f01a4cadf75685f24303e
Goodwin Ogbuehi <github@gogbuehi.com>
2018-06-06 19:53:21 -0400

    ICC-5 Primes Test: Create Primes Generator

--- app/utils/primes-generator.js
+++ app/utils/primes-generator.js
@@ -1,12 +1,31 @@
 // Utility for Generating Primes
 var exports = module.exports = {};
 
-
+/**
+ * Method to generate an array of primes
+ * @param   {int} n   The number of primes to generate
+ * @returns {int[]}   An ordered array of the first n primes
+ */
 exports.generateNPrimes = (n) => {
+  if (n < 1) return [];
   let   lastPrime    = 2;
   let   currentValue = lastPrime;
   const primesArray  = [lastPrime];
+  let currentPrime, isPrime;
+  for (let i = 1; primesArray.length < n; i++) {
+    ++currentValue;
+    isPrime = true;
+    for (let j = 0; j < primesArray.length; j++) {
+      currentPrime = primesArray[j];
+      if ((currentValue % currentPrime)=== 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primesArray.push(currentValue);
+    }
+  }
 
-
-  return [];
+  return primesArray;
 };
--- test/utils/primes-generator-test.js
+++ test/utils/primes-generator-test.js
@@ -3,6 +3,10 @@ const assert          = require('assert');
 const PrimesGenerator = locreq('app/utils/primes-generator');
 
 
+const CONSTANTS = {
+  tenPrimes: [2,3,5,7,11,13,17,19,23,29]
+}
+
 const generateNPrimes = {
   it: {
     shouldReturnArrayLengthOfN: () => {
@@ -11,12 +15,19 @@ const generateNPrimes = {
         nPrimes = PrimesGenerator.generateNPrimes(i);
         assert.equal(nPrimes.length, i);
       }
+    },
+    shouldReturnArrayOfNPrimes: () => {
+      const n = 10;
+      let nPrimes = PrimesGenerator.generateNPrimes(n);
+
+      assert.deepEqual(CONSTANTS.tenPrimes, nPrimes);
     }
   }
 }
 
 const describeGenerateNPrimes = () => {
   it('should return an array of length n', generateNPrimes.it.shouldReturnArrayLengthOfN);
+  it('should return an array of n primes', generateNPrimes.it.shouldReturnArrayOfNPrimes);
 };
 const describePrimesGenerator = () => {
   describe('#generateNPrimes()', describeGenerateNPrimes);
