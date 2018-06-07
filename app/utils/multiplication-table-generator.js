11cb0e6fc8c6c6c13122f6bf0cfc22b4acd98b7d
Goodwin Ogbuehi <github@gogbuehi.com>
2018-06-06 23:28:29 -0400

    ICC-6 Primes Test: Create multiplication table method  - Put in Multiplication Table generator

--- app/utils/multiplication-table-generator.js
+++ app/utils/multiplication-table-generator.js
@@ -1,7 +1,12 @@
 // Utility for Generating Multiplication Table Data
 var exports = module.exports = {};
 
-exports.multiplicationMatrix = (intArray) => {
+/**
+ * Generate a matrix of all multiples from an array of factors
+ * @param   {int[]}   intArray  The factors to use
+ * @returns {int[][]} A matrix of all the multiples
+ */
+exports.multiplicationMatrix = function(intArray) {
   const matrix = [];
 
   for(let i = 0; i < intArray.length; i++) {
@@ -13,3 +18,21 @@ exports.multiplicationMatrix = (intArray) => {
 
   return matrix;
 };
+
+/**
+ * Generate a matrix that represents a multiplication table
+ * @param   {int[]}   intArray  The factors to use
+ * @returns {int[][]} A matrix with a factors row and column,
+ *                    and the matrix of the multiples.
+ */
+exports.multiplicationTable = (intArray) => {
+  if (intArray.length === 0) return [];
+  const matrix = exports.multiplicationMatrix(intArray);
+  let intArrayToDisplay = [''].concat(intArray);
+  matrix.unshift(intArrayToDisplay);
+
+  for (let i = 1; i < intArrayToDisplay.length; i++) {
+    matrix[i].unshift(intArrayToDisplay[i]);
+  }
+  return matrix;
+}
--- test/utils/multiplication-table-generator-test.js
+++ test/utils/multiplication-table-generator-test.js
@@ -29,17 +29,62 @@ const multiplicationMatrix = {
       }
     }
   }
+};
+
+const multiplicationTable = {
+  it: {
+    shouldReturnEmptyArray: () => {
+      const table = MultiplicationTableGenerator.multiplicationTable([]);
+      assert.equal(table.length, 0);
+    },
+    shouldReturnAMatrixOneGreater: () => {
+      const tableSize = CONSTANTS.intArray.length + 1;
+      const table = MultiplicationTableGenerator.multiplicationTable(CONSTANTS.intArray);
+      assert.equal(table.length, tableSize);
+      for (let i = 0; i < table.length; i++) {
+        assert.equal(table[i].length, tableSize);
+      }
+    },
+    shouldReturnTableWithFactors: () => {
+      const table = MultiplicationTableGenerator.multiplicationTable(CONSTANTS.intArray);
+      assert.equal(table[0][0], '', 'Table[0,0] should be blank.');
+      for(let i = 0; i < CONSTANTS.intArray.length; i++) {
+        assert.equal(table[0][i+1], CONSTANTS.intArray[i], 'Table Row has an unexpected factor.');
+        assert.equal(table[i+1][0], CONSTANTS.intArray[i], 'Table Column has an unexpected factor.');
+      }
+    },
+    shouldReturnTableWithMultiples: () => {
+      const table = MultiplicationTableGenerator.multiplicationTable(CONSTANTS.intArray);
+      const matrix = MultiplicationTableGenerator.multiplicationMatrix(CONSTANTS.intArray);
+
+      let values;
+      for(let i = 0; i < matrix.length; i++) {
+        values = matrix[i];
+        for(let j = 0; j < values.length; j++) {
+          assert.equal(matrix[i][j], table[i+1][j+1], `Table[${i+1},${j+1}] doesn't match Matrix[${i},${j}].`);
+        }
+      }
+    }
+  }
 }
 
 const describeMultiplicationMatrix = () => {
   it('should return an empty array, if provided an empty array', multiplicationMatrix.it.shouldReturnEmptyArray);
   it('should return a 2-dimensional array, if provided an array of values', multiplicationMatrix.it.shouldReturn2DArray);
   it('should return a matrix of multiples of the provided array of integers', multiplicationMatrix.it.shouldReturnMultiplicationMatrix);
-}
+};
+
+const describeMultiplicationTable = () => {
+  it('should return an empty array, if provided an empty array', multiplicationTable.it.shouldReturnEmptyArray);
+  it('should return a matrix one greater than the size of the provided array', multiplicationTable.it.shouldReturnAMatrixOneGreater);
+  it('should return a table with a factors row and column added', multiplicationTable.it.shouldReturnTableWithFactors);
+  it('should return a table with the multiples provided by the matrix', multiplicationTable.it.shouldReturnTableWithMultiples);
+};
 
 
 const describeMutliplicationTableGenerator = () => {
   describe('#multiplicationMatrix()', describeMultiplicationMatrix);
+  describe('#multiplicationTable()', describeMultiplicationTable);
 };
 
 describe('MultiplicationTableGenerator', describeMutliplicationTableGenerator);
