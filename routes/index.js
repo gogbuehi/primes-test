36a092bb956837ad1fb2c56b5ec19ed3c038ab6d
Goodwin Ogbuehi <github@gogbuehi.com>
2018-06-07 00:44:08 -0400

    ICC-7 Primes Test: Create simple interface for interacting with Primes Table

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
+const locreq          = require('locreq')(__dirname);
+const PrimesGenerator = locreq('app/utils/primes-generator');
+const MultiplicationTableGenerator = locreq('app/utils/multiplication-table-generator');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let message = '';
+  let n = Math.abs(parseInt(req.query.n || 0));
+  const startTime = new Date().getTime();
+  const primes = PrimesGenerator.generateNPrimes(n);
+  const endTime = new Date().getTime();
+
+  console.log('Prime Generation Runtime: ', endTime-startTime);
+
+  const table = MultiplicationTableGenerator.limitedMultiplicationTable(primes);
+
+  res.render('index', {
+    title: 'Primes Table',
+    timeRunning: endTime-startTime,
+    n,
+    primes,
+    table,
+    message
+  });
 });
 
 module.exports = router;
