992d1f3ccaca0018b6fcbeab92e724be2b87efaf
Goodwin Ogbuehi <github@gogbuehi.com>
2018-06-06 21:22:52 -0400

    ICC-6 Primes Test: Create multiplication table method  - Implementation and initial tests

--- app/utils/multiplication-table-generator.js
+++ app/utils/multiplication-table-generator.js
@@ -2,5 +2,14 @@
 var exports = module.exports = {};
 
 exports.multiplicationMatrix = (intArray) => {
-  return [];
+  const matrix = [];
+
+  for(let i = 0; i < intArray.length; i++) {
+    matrix[i] = [];
+    for(let j = 0; j < intArray.length; j++) {
+      matrix[i][j] = intArray[i] * intArray[j];
+    }
+  }
+
+  return matrix;
 };
--- test/utils/multiplication-table-generator-test.js
+++ test/utils/multiplication-table-generator-test.js
@@ -3,7 +3,14 @@ const assert                       = require('assert');
 const MultiplicationTableGenerator = locreq('app/utils/multiplication-table-generator');
 
 const CONSTANTS = {
-  intArray : [2,3,5,7,11]
+  intArray : [2,3,5,7,11],
+  multiplicationMatrix : [
+    [  4,  6, 10, 14, 22],
+    [  6,  9, 15, 21, 33],
+    [ 10, 15, 25, 35, 55],
+    [ 14, 21, 35, 49, 77],
+    [ 22, 33, 55, 77,121]
+  ]
 }
 
 const multiplicationMatrix = {
@@ -16,6 +23,10 @@ const multiplicationMatrix = {
       const matrix = MultiplicationTableGenerator.multiplicationMatrix(CONSTANTS.intArray);
       assert.equal(matrix.length, CONSTANTS.intArray.length);
       assert.equal(matrix[0].length, CONSTANTS.intArray.length);
+    },
+    shouldReturnMultiplicationMatrix: () => {
+      const matrix = MultiplicationTableGenerator.multiplicationMatrix(CONSTANTS.intArray);
+      assert.deepEqual(matrix, CONSTANTS.multiplicationMatrix);
     }
   }
 }
@@ -23,11 +34,12 @@ const multiplicationMatrix = {
 const describeMultiplicationMatrix = () => {
   it('should return an empty array, if provided an empty array', multiplicationMatrix.it.shouldReturnEmptyArray);
   it('should return a 2-dimensional array, if provided an array of values', multiplicationMatrix.it.shouldReturn2DArray);
+  it('should return a matrix of multiples of the provided array of integers', multiplicationMatrix.it.shouldReturnMultiplicationMatrix);
 }
 
 
-const describeMutltiplicationTableGenerator = () => {
+const describeMutliplicationTableGenerator = () => {
   describe('#multiplicationMatrix()', describeMultiplicationMatrix);
 };
 
-describe('MultiplicationTableGenerator', describeMutltiplicationTableGenerator);
+describe('MultiplicationTableGenerator', describeMutliplicationTableGenerator);
