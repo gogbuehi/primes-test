c4faab9448f25be638faf0cbcbabc5e17e4ec227
Goodwin Ogbuehi <github@gogbuehi.com>
2018-06-06 20:53:30 -0400

    ICC-5 Primes Test: Create Primes Generator  - Refactoring for better testing

--- app/utils/primes-generator.js
+++ app/utils/primes-generator.js
@@ -1,28 +1,44 @@
 // Utility for Generating Primes
 var exports = module.exports = {};
 
+
+/**
+ * Method to test whether a value is prime
+ * Note: This depends on an array of all primes
+ *       up to the value provided.
+ * @param   {int}       value         The value to test
+ * @param   {int[]}     primesArray   An array of prime numbers
+ * @returns {boolean}   False, if the value is divisible by a
+ *                      number in the array
+ *                      True, if the value appears to be prime
+ *
+ */
+exports.primeTester = (value, primesArray) => {
+  let currentPrime;
+  for (let i = 0; i < primesArray.length; i++) {
+    currentPrime = primesArray[i];
+    if (value === currentPrime) return true;
+    if ((value % currentPrime) === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 /**
  * Method to generate an array of primes
- * @param   {int} n   The number of primes to generate
- * @returns {int[]}   An ordered array of the first n primes
+ * @param   {int}   n   The number of primes to generate
+ * @returns {int[]}     An ordered array of the first n primes
  */
-exports.generateNPrimes = (n) => {
+exports.generateNPrimes = function(n) {
   if (n < 1) return [];
   let   lastPrime    = 2;
   let   currentValue = lastPrime;
   const primesArray  = [lastPrime];
-  let currentPrime, isPrime;
+
   for (let i = 1; primesArray.length < n; i++) {
-    ++currentValue;
-    isPrime = true;
-    for (let j = 0; j < primesArray.length; j++) {
-      currentPrime = primesArray[j];
-      if ((currentValue % currentPrime)=== 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    if (isPrime) {
+
+    if (exports.primeTester(++currentValue, primesArray)) {
       primesArray.push(currentValue);
     }
   }
--- test/utils/primes-generator-test.js
+++ test/utils/primes-generator-test.js
@@ -4,8 +4,27 @@ const PrimesGenerator = locreq('app/utils/primes-generator');
 
 
 const CONSTANTS = {
-  tenPrimes: [2,3,5,7,11,13,17,19,23,29]
-}
+  notAPrime1   : 4,
+  notAPrime2   : 30,
+  tenPrimes    : [2,3,5,7,11,13,17,19,23,29],
+  aBiggerPrime : 31
+};
+
+const primeTester = {
+  it: {
+    shouldReturnFalseIfNotPrime: () => {
+      assert.equal(PrimesGenerator.primeTester(CONSTANTS.notAPrime1, CONSTANTS.tenPrimes), false);
+      assert.equal(PrimesGenerator.primeTester(CONSTANTS.notAPrime2, CONSTANTS.tenPrimes), false);
+    },
+    shouldReturnTrueIfPrime: () => {
+      for (let i = 0; i < CONSTANTS.tenPrimes.length; i++) {
+        assert(PrimesGenerator.primeTester(CONSTANTS.tenPrimes[i], CONSTANTS.tenPrimes));
+      }
+
+      assert(PrimesGenerator.primeTester(CONSTANTS.aBiggerPrime, CONSTANTS.tenPrimes));
+    }
+  }
+};
 
 const generateNPrimes = {
   it: {
@@ -25,11 +44,17 @@ const generateNPrimes = {
   }
 }
 
+const describePrimeTester = () => {
+  it('should return false, if value is not prime', primeTester.it.shouldReturnFalseIfNotPrime);
+  it('should return true, if value is prime', primeTester.it.shouldReturnTrueIfPrime);
+};
 const describeGenerateNPrimes = () => {
   it('should return an array of length n', generateNPrimes.it.shouldReturnArrayLengthOfN);
   it('should return an array of n primes', generateNPrimes.it.shouldReturnArrayOfNPrimes);
 };
+
 const describePrimesGenerator = () => {
+  describe('#primeTester()', describePrimeTester);
   describe('#generateNPrimes()', describeGenerateNPrimes);
 };
 
